09df1b0b3adb59287583f662255e3c3e88d277a9
mdupree <mdupree@acrocommerce.com>
2025-04-17 08:35:45 -0700

    revert: Adds notes directory browsing/editing

--- lua/nvim-notes/commands.lua
+++ lua/nvim-notes/commands.lua
@@ -18,26 +18,6 @@ function M.setup()
 			cwd = config.get_notes_dir(),
 		})
 	end, {})
-
-	-- New ViewNotes command
-	vim.api.nvim_create_user_command("ViewNotes", function()
-		-- Get the user-defined notes directory
-		local notes_directory = config.get_notes_dir()
-
-		-- Create a vertical split
-		vim.cmd("vsplit")
-
-		-- Focus the left window for Neo-tree
-		vim.cmd("wincmd h")
-		require("neo-tree").open() -- Open Neo-tree in the left pane
-
-		-- Focus the right window for file viewing
-		vim.cmd("wincmd l")
-		vim.cmd("Explore " .. notes_directory) -- Open the notes directory in the right window
-
-		-- Optional: Set Neo-tree window width (you can tweak this as needed)
-		vim.wo[vim.fn.winnr()].winwidth = 30
-	end, {})
 end
 
 return M
